9e1e427ecbf68452db34576a4459f00dec7878a9
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-08-26 18:20:06 +0700

    Cat layout header, section, footer thanh cong, nhung backend Loai san pham

---
+++ Controllers/ModuleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopThoiTrang.Models;
+
+namespace ShopThoiTrang.Controllers
+{
+    public class ModuleController : Controller
+    {
+        private ShopThoiTrangDBContext db = new ShopThoiTrangDBContext();
+
+        // GET: Module
+        public ActionResult ListCategoryMenu()
+        {
+            var list = db.Categorys.Where(m => m.ParentId == 0 && m.Status == 1).ToList();
+            return View("ListCategoryMenu", list);
+        }
+    }
+}
